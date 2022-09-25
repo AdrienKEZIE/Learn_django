910a45c69146ce3d8671c541687195803ad21675
Adrien <adrienkezie03@gmail.com>
2022-09-25 20:07:25 +0000

    models adding

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blog, contact
 
-# Register your models here.
+admin.site.register(Blog)
+admin.site.register(contact)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Blog
 def home(request):
-    return render(request, 'core/home.html')
+    context = {'posts': Blog.objects.all()}
+    return render(request, 'core/home.html', context=context)
 def about(request):
     return render(request, 'core/about.html')
 def contact(request):
+
     return render(request, 'core/contact.html')
